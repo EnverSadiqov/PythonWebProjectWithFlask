7e4bb6bdff943e96ca9687f36bc4804509d748ea
sadiqov enver <b-l-i-m@mail.ru>
2022-09-28 00:19:03 +0400

    Flask Sqlite register

--- run.py
+++ run.py
@@ -9,14 +9,34 @@ def home ():
 @app.route('/demo',methods=["GET",'POST'])
 def demo():
     if request.method=='POST':
-        _username=name=request.form['u_name']
-        _password=name=request.form['u_pass']
+        username=name=request.form['u_name']
+        password=name=request.form['u_pass']
         conn =sqlite3.connect('project.db')
-        query=f"INSERT INTO Workers (u_name,u_pass) VALUES ('{_username}','{_password}')" 
+        query=f"INSERT INTO Workers (u_name,u_pass) VALUES ('{username}','{password}')" 
         conn.execute(query)
         conn.commit()
         
     return render_template ('demo.html')
+
+@app.route('/delete/<id>')
+def delete(id):
+    conn = sqlite3.connect('project.db')
+    query = f"delete from user where id = {id}"
+    conn.execute(query)
+    conn.commit()
+    
+    
+@app.route('/update/<id>',methods=['GET','POST'])
+def update(id):
+    conn = sqlite3.connect('project.db')
+    if request.method=='POST':
+        username = request.form.get('username')
+        password = request.form.get('password')  
+        conn.execute("UPDATE user set username=?,usermail =? WHERE ID=?",[username, password,id])
+        conn.commit()
+        print('dogru')
+    return render_template('update.html',x=id)
+
 
 if __name__=='__main__':
     app.run(port=5000,debug=True)
