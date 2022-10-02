560b029fc2f639c2786e03f8a30272f6f06c16e2
sadiqov enver <b-l-i-m@mail.ru>
2022-10-02 16:33:36 +0400

    .

---
+++ run1.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+class Customers(db.Model):
+    id = db.Column(db.Integer,primary_key=True,)
+    name = db.Column(db.String(100))
+    mess = db.Column(db.String(200))
+
+@app.route('/')
+def home():
+    name = request.args.get('u_name')
+    mess = request.args.get('u_message')
+    customer = Customers(name=name,mess=mess)
+    db.session.add(customer)
+    db.session.commit()
+    return render_template('index1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
