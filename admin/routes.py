a44cd171581edf608299af7c321995dc256f25a2
sadiqov enver <b-l-i-m@mail.ru>
2022-11-12 03:14:05 +0400

    -

--- admin/routes.py
+++ admin/routes.py
@@ -6,3 +6,22 @@ from admin.forms import ProductForm
 def admin_index():
     return render_template('admin/index.html')
 
+
+
+@admin_bp.route('/products',methods=['GET','POST'] )
+def admin_products():
+    from start import db
+    from models import Product
+    products=Product.query.all()
+    productform=ProductForm()
+    if request.method=='POST':
+        name=productForm.name.data
+        price=productForm.price.data
+        amount=productForm.amount.data
+        info=productForm.info.data
+        product=Product(name=name,price=price,amount=amount,info=info)
+        db.session.add(product)
+        db.session.commit() 
+        return redirect ('/admin/products')
+    return render_template('admin/products.html', productform=productsform,products=products) 
+
--- start.py
+++ start.py
@@ -1,17 +1,23 @@
 from flask import Flask ,request, render_template, redirect,url_for,flash, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 main=Flask(__name__)
+main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+main.config['SECRET_KEY'] ="secretkey"
+db=SQLAlchemy(main)
+migrate=Migrate(main,db)
 
+
+from models import *
+from app.routes import *
+from admin.routes import *
 
 #import Blueprint
 from app import app_bp
 from admin import admin_bp
-from app.routes import *
-from admin.routes import *
 
 #register Blueprint
-
 main.register_blueprint(app_bp)
 main.register_blueprint(admin_bp)
 
